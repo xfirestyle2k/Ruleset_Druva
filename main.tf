048519b2e7b2eeb0a9f411773e702d22ccbe88ac
Thomas Sauerer <tsauerer@tsauerer-a01.vmware.com>
2020-06-11 13:42:58 +0200

    basic commit

---
+++ main.tf
@@ -0,0 +1,195 @@
+provider "nsxt" {
+  host                 = var.host
+  vmc_token            = var.vmc_token
+  allow_unverified_ssl = true
+  enforcement_point    = "vmc-enforcementpoint"
+}
+
+
+###################### creating Management Gateway Firewall rule ######################
+
+resource "nsxt_policy_gateway_policy" "mgw_policy" {
+  category     = "LocalGatewayRules"
+  display_name = "default"
+  domain       = "mgw"
+  rule {
+    action = "ALLOW"
+    destination_groups = ["/infra/domains/mgw/groups/VCENTER"]
+    destinations_excluded = false
+    direction             = "IN_OUT"
+    disabled              = false
+    display_name          = "vCenter Inbound set up by Terraform"
+    ip_version            = "IPV4_IPV6"
+    logged                = false
+    profiles              = []
+    scope = [
+      "/infra/labels/mgw",
+    ]
+    services = [
+      "/infra/services/HTTPS",
+      "/infra/services/ICMP-ALL",
+    ]
+    source_groups    = ["${nsxt_policy_group.Druva_Proxy.path}"]
+    sources_excluded = false
+  }
+}
+
+###################### creating Compute Gateway Firewall rule ######################
+
+resource "nsxt_policy_gateway_policy" "cgw_policy" {
+  category     = "LocalGatewayRules"
+  display_name = "default"
+  domain       = "cgw"
+  rule {
+    action = "ALLOW"
+    destination_groups = ["/infra/domains/mgw/groups/VCENTER"]
+    destinations_excluded = false
+    direction             = "IN_OUT"
+    disabled              = false
+    display_name          = "vCenter Inbound set up by Terraform"
+    ip_version            = "IPV4_IPV6"
+    logged                = false
+    profiles              = []
+    scope = [
+      "/infra/labels/cgw",
+    ]
+    services = [
+      "/infra/services/HTTPS",
+      "/infra/services/ICMP-ALL",
+    ]
+    source_groups    = ["${nsxt_policy_group.Druva_Proxy.path}"]
+    sources_excluded = false
+  }
+  rule {
+    action = "ALLOW"
+    destination_groups = []
+    destinations_excluded = false
+    direction             = "IN_OUT"
+    disabled              = false
+    display_name          = "Druva Internet access set up by Terraform"
+    ip_version            = "IPV4_IPV6"
+    logged                = false
+    profiles              = []
+    scope = [
+      "/infra/labels/cgw",
+    ]
+    services = [
+      "/infra/services/HTTPS"
+    ]
+    source_groups    = ["${nsxt_policy_group.Druva_Proxy.path}"]
+    sources_excluded = false
+  }
+}
+
+###################### creating Groups ######################
+
+// creating Group for Druva_Proxy:
+resource "nsxt_policy_group" "Druva_Proxy" {
+  display_name = "Druva_Proxy"
+  description  = "Created from Terraform Druva_Proxy"
+  domain       = "cgw"
+}
+
+// creating Group for Druva_Cache:
+resource "nsxt_policy_group" "Druva_Cache" {
+  display_name = "Druva_Cache"
+  description  = "Created from Terraform Druva_Cache"
+  domain       = "cgw"
+}
+
+// creating Group for RFC_1918:
+resource "nsxt_policy_group" "RFC_1918" {
+  display_name = "RFC_1918"
+  description  = "Created from Terraform RFC_1918"
+  domain       = "cgw"
+
+    criteria {
+    ipaddress_expression {
+      ip_addresses = ["192.168.0.0/16", "172.16.0.0/16", "10.0.0.0/8"]
+    }
+  }
+}
+
+// creating Group for vCenter:
+resource "nsxt_policy_group" "vCenter" {
+  display_name = "vCenter"
+  description  = "Created from Terraform vCenter"
+  domain       = "cgw"
+}
+
+###################### creating Services ######################
+
+// creating Services TCP 8443:
+resource "nsxt_policy_service" "Druva_Restore_Port" {
+  description  = "Druva_Restore_Port provisioned by Terraform"
+  display_name = "Druva_Restore_Port TCP3542"
+
+  l4_port_set_entry {
+    display_name      = "TCP3542"
+    description       = "TCP port 3542 entry"
+    protocol          = "TCP"
+    destination_ports = ["3542"]
+  }
+}
+
+###################### creating DFW Security Rules ######################
+
+###################### creating Ruleset for Cloud Proxy ######################
+
+resource "nsxt_policy_security_policy" "Druva_Proxy" {
+  domain       = "cgw"
+  display_name = "Druva_Proxy"
+  description  = "Terraform Druva_Proxy Ruleset"
+  category     = "Application"
+  rule {
+    display_name       = "Allow_vCenter_outbound"
+    source_groups      = ["${nsxt_policy_group.Druva_Proxy.path}"]
+    destination_groups = ["${nsxt_policy_group.vCenter.path}"]
+    action             = "ALLOW"
+    services           = ["/infra/services/HTTPS"]
+    logged             = true
+  }
+  rule {
+    display_name       = "Allow_vCenter_inbound"
+    source_groups      = ["${nsxt_policy_group.vCenter.path}"]
+    destination_groups = ["${nsxt_policy_group.Druva_Proxy.path}"]
+    action             = "ALLOW"
+    services           = ["/infra/services/HTTPS"]
+    logged             = true
+  }
+  rule {
+    display_name       = "Allow_Restore_Outbound"
+    source_groups      = ["${nsxt_policy_group.Druva_Proxy.path}"]
+    destination_groups = ["${nsxt_policy_group.RFC_1918.path}"]
+    action             = "ALLOW"
+    services           = ["${nsxt_policy_service.Druva_Restore_Port.path}"]
+    logged             = true
+  }
+  rule {
+    display_name       = "Internet Access"
+    source_groups      = ["${nsxt_policy_group.Druva_Proxy.path}"]
+    destination_groups = ["${nsxt_policy_group.RFC_1918.path}"]
+    destinations_excluded = true
+    action             = "ALLOW"
+    services           = ["/infra/services/HTTPS"]
+    logged             = true
+  }
+  rule {
+    display_name       = "Deny_Traffic_Outbound"
+    source_groups      = ["${nsxt_policy_group.Druva_Proxy.path}"]
+    destination_groups = ["${nsxt_policy_group.RFC_1918.path}"]
+    action             = "REJECT"
+    disabled           = true
+    services           = []
+    logged             = true
+  }
+  rule {
+      display_name       = "Deny_Traffic_Inbound"
+      source_groups      = ["${nsxt_policy_group.RFC_1918.path}"]
+      destination_groups = ["${nsxt_policy_group.Druva_Proxy.path}"]
+      action             = "REJECT"
+      disabled           = true
+      services           = []
+      logged             = true
+    }
+}
